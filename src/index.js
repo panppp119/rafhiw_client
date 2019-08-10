ad22b9c3a46f5796381e9e90e98af4d73b1cecfd
PanPPP <panppp@PanPPPs-MacBook-Pro.local>
2019-08-10 12:41:20 +0700

    add helmet, snapshot and change title name

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
 );
