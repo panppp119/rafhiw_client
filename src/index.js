1c13c5bd3a8cc02220e901610f1885e2afa09db8
PanPPP <panppp@PanPPPs-MacBook-Pro.local>
2019-08-25 10:43:12 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+// import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom'
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-render(
+ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
