e3eca8ccf6ac9249435625e5be75f5d865bf5964
Phitchaporn Pipatpunlop <phitchaporn.pipa@gmail.com>
2020-07-19 21:46:25 +0700

    fix price not show

--- src/components/cards/ProductCard/ProductCard.js
+++ src/components/cards/ProductCard/ProductCard.js
@@ -3,83 +3,95 @@ import { Link } from 'react-router-dom'
 import { List, Map } from 'immutable'
 
 import CountdownTimer from 'components/CountdownTimer'
-import PriceConvert from 'components/converts/PriceConvert';
+import PriceConvert from 'components/converts/PriceConvert'
 
 import './ProductCard.scss'
 
 class ProductCard extends React.Component {
   static defaultProps = {
-    product: Map()
+    product: Map(),
   }
 
-  render () {
+  render() {
     const { product } = this.props
 
-    const options =
+    let options =
       (product.get('options') &&
         product.get('options').sort((a, b) => {
           if (a.get('discount_amt') !== 0) {
-            return a.get('discount_amt') - b.get('discount_amt');
+            return a.get('discount_amt') - b.get('discount_amt')
           } else {
-            return a.get('price_amt') - b.get('price_amt');
+            return a.get('price_amt') - b.get('price_amt')
           }
         })) ||
-      List();
-    var noItem = options.filter(opt => opt.get('stock') <= 0).size > 0
+      List()
+    var noItem = options.filter((opt) => opt.get('stock') <= 0).size > 0
 
-    options.map(option => {
-      const discount = option.get('discount_amt') !== 0 ?
-        option.get('discount_amt') + option.get('ship_amt') + option.get('hiw_amt')
-        : option.get('price_amt') + option.get('ship_amt') + option.get('hiw_amt')
-      const original = option.get('price_amt') + option.get('ship_amt') + option.get('hiw_amt')
-      const originalIncludeVat = original + (original * (15 / 100)) || 0
-      const discountIncludeVat = discount + (discount * (15 / 100)) || 0
-      
-      return {
-        ...option,
+    options = options.map((option) => {
+      const discount =
+        option.get('discount_amt') !== 0
+          ? option.get('discount_amt') +
+            option.get('ship_amt') +
+            option.get('hiw_amt')
+          : option.get('price_amt') +
+            option.get('ship_amt') +
+            option.get('hiw_amt')
+      const original =
+        option.get('price_amt') + option.get('ship_amt') + option.get('hiw_amt')
+      const originalIncludeVat = original + original * (15 / 100) || 0
+      const discountIncludeVat = discount + discount * (15 / 100) || 0
+
+      let newOption = {
+        ...option.toJS(),
         originalIncludeVat,
-        discountIncludeVat
+        discountIncludeVat,
       }
+
+      return newOption
     })
 
     return (
-      <div id="product-card">
+      <div id='product-card'>
         <Link to={`/p/${product.get('id')}`}>
-          <div className="image"
+          <div
+            className='image'
             style={{
-              backgroundImage: `url(${product.get('image') || process.env.REACT_APP_DEFAULT_IMAGE})`
+              backgroundImage: `url(${
+                product.get('image') || process.env.REACT_APP_DEFAULT_IMAGE
+              })`,
             }}
             aria-label={product.get('name')}
           >
-            {
-              noItem && (
-                <div className="no-item">
-                  สินค้าหมด
-                </div>
-              )
-            }
+            {noItem && <div className='no-item'>สินค้าหมด</div>}
           </div>
 
-          <div className="content">
+          <div className='content'>
             <CountdownTimer item={product} />
 
-            <h4 className="name">{product.get('name')}</h4>
-            <p className="price">
-              {!options.isEmpty() && options.getIn([0, 'discount_amt']) !== 0 ? (
+            <h4 className='name'>{product.get('name')}</h4>
+            <p className='price'>
+              {!options.isEmpty() &&
+              options.getIn([0, 'discount_amt']) !== 0 ? (
                 <Fragment>
                   <span
-                    className="price"
+                    className='price'
                     style={{ textDecoration: 'line-through' }}
                   >
-                    <PriceConvert price={options.getIn([0, 'originalIncludeVat'])} />
+                    <PriceConvert
+                      price={options.getIn([0, 'originalIncludeVat'])}
+                    />
                   </span>
 
                   <span className='discount'>
-                    <PriceConvert price={options.getIn([0, 'discountIncludeVat'])} />
+                    <PriceConvert
+                      price={options.getIn([0, 'discountIncludeVat'])}
+                    />
                   </span>
                 </Fragment>
               ) : (
-                <PriceConvert price={options.getIn([0, 'originalIncludeVat'])} />
+                <PriceConvert
+                  price={options.getIn([0, 'originalIncludeVat'])}
+                />
               )}
             </p>
           </div>
--- src/components/forms/EventForm/EventForm.js
+++ src/components/forms/EventForm/EventForm.js
@@ -1,101 +1,103 @@
-import React from 'react';
-import DatePicker from 'react-datepicker';
-import DropzoneComponent from 'react-dropzone-component';
+import React from 'react'
+import DatePicker from 'react-datepicker'
+import DropzoneComponent from 'react-dropzone-component'
 import Classnames from 'classnames'
 
-import GMap from 'components/GMap';
+import GMap from 'components/GMap'
 
-import './EventForm.scss';
+import './EventForm.scss'
 
-var myDropzone;
+var myDropzone
 
 class EventForm extends React.Component {
   render() {
-    const { events, e1, e2, e6, e7 } = this.props;
+    const { events, e1, e2, e6, e7 } = this.props
 
     var eventOptions = []
 
     !events.isEmpty() &&
-      events.map(event =>
+      events.map((event) =>
         eventOptions.push({
           text: event.get('name'),
-          value: event.get('id')
-        })
-      );
+          value: event.get('id'),
+        }),
+      )
 
     var previewConfig = {
       iconFiletypes: ['.jpg', '.png'],
       showFiletypeIcon: false,
-      postUrl: 'no-url'
-    };
+      postUrl: 'no-url',
+    }
 
     var djsConfig = {
       autoProcessQueue: false,
       addRemoveLinks: true,
-      maxFiles: 1
-    };
+      maxFiles: 1,
+    }
 
     var eventHandlers = {
-      init: dropzone => (myDropzone = dropzone),
-      addedfile: file => this.props.addEventFile(file),
-      removedfile: file => this.props.removeFile(file, 'event')
-    };
+      init: (dropzone) => (myDropzone = dropzone),
+      addedfile: (file) => this.props.addEventFile(file),
+      removedfile: (file) => this.props.removeFile(file, 'event'),
+    }
 
     return (
-      <div className="add-event">
-        <div className="form-field">
+      <div className='add-event'>
+        <div className='form-field'>
           <label>ชื่องาน</label>
-          <input type="text"
-            name="name"
+          <input
+            type='text'
+            name='name'
             onChange={this.props.handleChangeEvent}
-            autoComplete="off"
+            autoComplete='off'
             className={Classnames({ error: e1 })}
           />
         </div>
 
-        <div className="form-field">
+        <div className='form-field'>
           <label>รายละเอียดงาน</label>
-          <textarea name="description"
+          <textarea
+            name='description'
             onChange={this.props.handleChangeEvent}
             className={Classnames({ error: e2 })}
           />
         </div>
 
-        <div className="form-group">
-          <div className="form-field">
+        <div className='form-group'>
+          <div className='form-field'>
             <label>เริ่มต้น</label>
             <DatePicker
               selected={this.props.event.start_date}
-              onChange={date =>
+              onChange={(date) =>
                 this.props.handleChangeEventDate(date, 'start_date')
               }
               peekNextMonth
               showTimeSelect
-              dropdownMode="select"
-              dateFormat="dd/MM/yy HH:mm"
+              dropdownMode='select'
+              dateFormat='dd/MM/yy HH:mm'
               className={Classnames({ error: e6 })}
             />
           </div>
 
-          <div className="form-field">
+          <div className='form-field'>
             <label>สิ้นสุด</label>
             <DatePicker
               selected={
                 this.props.event.end_date || this.props.event.start_date
               }
-              onChange={date =>
+              onChange={(date) =>
                 this.props.handleChangeEventDate(date, 'end_date')
               }
               peekNextMonth
               showTimeSelect
-              dropdownMode="select"
-              dateFormat="dd/MM/yy HH:mm"
+              dropdownMode='select'
+              dateFormat='dd/MM/yy HH:mm'
               className={Classnames({ error: e7 })}
             />
           </div>
         </div>
 
-        <div className="form-field">
+        <div className='form-field'>
           <DropzoneComponent
             config={previewConfig}
             eventHandlers={eventHandlers}
@@ -103,13 +105,13 @@ class EventForm extends React.Component {
           />
         </div>
 
-        <div className="form-field">
+        <div className='form-field'>
           <label>สถานที่จัดงาน</label>
           <GMap place onCenterChanged={this.props.onCenterChanged} />
         </div>
 
-        <div className="form-field">
-          <button className='primary' onClick={this.props.submitEvent} >
+        <div className='form-field'>
+          <button className='primary' onClick={this.props.submitEvent}>
             เพิ่มงาน
           </button>
 
@@ -118,8 +120,8 @@ class EventForm extends React.Component {
           </button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default EventForm;
+export default EventForm
