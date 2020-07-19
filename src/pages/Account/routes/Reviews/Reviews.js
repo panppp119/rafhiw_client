817e5ad00644192f4515ed5613e29eb0cd6decad
Phitchaporn Pipatpunlop <phitchaporn.pipa@gmail.com>
2020-07-19 22:36:56 +0700

    add star

--- src/pages/Account/routes/Reviews/Reviews.js
+++ src/pages/Account/routes/Reviews/Reviews.js
@@ -1,80 +1,117 @@
-import React from 'react';
-import Numeral from 'numeral';
+import React from 'react'
+import Numeral from 'numeral'
 import ClassNames from 'classnames'
 import { Map } from 'immutable'
 import { FaStar } from 'react-icons/fa'
 
-import './Reviews.scss';
+import './Reviews.scss'
 
 class Reviews extends React.Component {
   componentDidMount() {
     !this.props.user.isEmpty() &&
-      (this.props.reviews.isEmpty() &&
-        this.props.loadReviews({ seller_id: this.props.user.get('id') }));
+      this.props.reviews.isEmpty() &&
+      this.props.loadReviews({ seller_id: this.props.user.get('id') })
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.reviews.isEmpty() && prevProps.user !== this.props.user) {
-      this.props.loadReviews({ seller_id: this.props.user.get('id') });
+      this.props.loadReviews({ seller_id: this.props.user.get('id') })
     }
   }
 
   render() {
-    const { reviews } = this.props;
+    const { reviews } = this.props
 
     var avg = 0
 
-    reviews.map(review =>
-      avg += review.get('rating')
-    )
+    reviews.map((review) => (avg += review.get('rating')))
 
-    avg = avg/reviews.size
+    avg = avg / reviews.size
 
     return (
-      <div className="reviews">
-        <div className="head">
+      <div className='reviews'>
+        <div className='head'>
           <h3>รีวิว</h3>
           <p>รีวิวทั้งหมด</p>
         </div>
 
-        <div className="body">
-          <div className="rating">
+        <div className='body'>
+          <div className='rating'>
             <h2>{Numeral(avg).format('0.00')} / 5</h2>
 
-            <div className="star">
-              <FaStar className={ClassNames({ active: avg >= 1 && avg <= 5 })} />
-              <FaStar className={ClassNames({ active: avg >= 2 && avg <= 5 })} />
-              <FaStar className={ClassNames({ active: avg >= 3 && avg <= 5 })} />
-              <FaStar className={ClassNames({ active: avg >= 4 && avg <= 5 })} />
+            <div className='star'>
+              <FaStar
+                className={ClassNames({ active: avg >= 1 && avg <= 5 })}
+              />
+              <FaStar
+                className={ClassNames({ active: avg >= 2 && avg <= 5 })}
+              />
+              <FaStar
+                className={ClassNames({ active: avg >= 3 && avg <= 5 })}
+              />
+              <FaStar
+                className={ClassNames({ active: avg >= 4 && avg <= 5 })}
+              />
               <FaStar className={ClassNames({ active: avg === 5 })} />
             </div>
           </div>
-          <div className="comments">
-            {
-              !reviews.isEmpty() ? reviews.map((review, i) => {
+          <div className='comments'>
+            {!reviews.isEmpty() ? (
+              reviews.map((review, i) => {
                 const user = review.get('user') || Map()
                 const product = review.get('product') || Map()
+                const rating = review.get('rating') || 0
 
                 return (
-                  <div className="comment" key={i}>
+                  <div className='comment' key={i}>
                     <img src={product.get('image')} alt={product.get('name')} />
 
-                    <div className="content">
+                    <div className='content'>
                       <h4>
                         {user.get('first_name')} {user.get('last_name')}
                       </h4>
-                      <p className='product'>({product.get('name')} - {product.get('po_name')})</p>
+                      <p className='product'>
+                        ({product.get('name')} - {product.get('po_name')})
+                      </p>
+                      <div
+                        className='star'
+                        style={{ fontSize: 12, marginBottom: 10 }}
+                      >
+                        <FaStar
+                          className={ClassNames({
+                            active: rating >= 1 && rating <= 5,
+                          })}
+                        />
+                        <FaStar
+                          className={ClassNames({
+                            active: rating >= 2 && rating <= 5,
+                          })}
+                        />
+                        <FaStar
+                          className={ClassNames({
+                            active: rating >= 3 && rating <= 5,
+                          })}
+                        />
+                        <FaStar
+                          className={ClassNames({
+                            active: rating >= 4 && rating <= 5,
+                          })}
+                        />
+                        <FaStar className={ClassNames({ active: avg === 5 })} />
+                      </div>
                       <p>{review.get('comment')}</p>
                     </div>
                   </div>
                 )
-              }) : <p>ยังไม่มีการรีิวิว</p>
-            }
+              })
+            ) : (
+              <p>ยังไม่มีการรีิวิว</p>
+            )}
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Reviews;
+export default Reviews
